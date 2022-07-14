db6c2f5478a3e42a80852cc4496f58f12bb85476
Kostadin Keljtanoski <keljtanoski@gmail.com>
2022-07-14 22:15:30 +0200

    Add ApiResponse language file and docker-compose.yml

---
+++ lang/en/apiResponse.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Api Responses
+    |--------------------------------------------------------------------------
+    |
+    | The following lines are used when Api response is provided.
+    |
+    */
+
+    'updateSuccess' => ':resource was updated successfully',
+    'storeSuccess' => ':resource was created successfully',
+    'deleteSuccess' => ':resource was deleted successfully',
+    'ok' => 'OK',
+    'success' => 'Success',
+];
